4b592536ce461cdd28ad1539af38649a3bbf42ba
SriGowriPati <119720222+SriGowriPati@users.noreply.github.com>
2023-01-03 23:05:09 +0530

    #include<stdio.h> int main() {     int a,i,b,gcd;     scanf("%d%d",&a,&b);     for(i=1;i<=a&&i<=b;i++)     {         if(a%i==0 && b%i==0)         {             gcd=i;         }     }     printf("%d",gcd); }

---
+++ Greatest_Common_Divisor.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,b,gcd;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a&&i<=b;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            gcd=i;
+        }
+    }
+    printf("%d",gcd);
+}
