bfe4a592a5912f8563723ddd7ef6d639bf8ebdb2
SriGowriPati <119720222+SriGowriPati@users.noreply.github.com>
2023-01-04 23:16:51 +0530

    #include<stdio.h> int main() {     int a,b,r,s=0;     scanf("%d",&a);     b=a*a;     while(b!=0)     {         r=b%10;         s=s+r;         b=b/10;     }     if(s==a)     {         printf("Neon Number");     }     else     {         printf("Not Neon Number");     } }

---
+++ Neon_Number.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,r,s=0;
+    scanf("%d",&a);
+    b=a*a;
+    while(b!=0)
+    {
+        r=b%10;
+        s=s+r;
+        b=b/10;
+    }
+    if(s==a)
+    {
+        printf("Neon Number");
+    }
+    else
+    {
+        printf("Not Neon Number");
+    }
+}
